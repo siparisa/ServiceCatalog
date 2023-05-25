741650d3c0de7c4a519d441825e9b1f31476a1fc
Parisa <siparisa@gmail.com>
2023-05-24 23:27:39 -0400

    add .env

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,12 +1,28 @@
 package db
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func InitDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=mysecretpassword dbname=postgres port=5432 sslmode=disable"
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
